e0dcc92388bbafa2f2920da3ccae37a7345abf5e
Arya Rizki Andaru <aryarizki3045@gmail.com>
2024-10-09 09:48:18 +0700

    create login and register

---
+++ app/src/main/java/com/example/inventarisapp/authentication/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.inventarisapp.authentication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.inventarisapp.R
+import com.example.inventarisapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
---
+++ app/src/main/java/com/example/inventarisapp/authentication/RegisterActivity.kt
@@ -0,0 +1,21 @@
+package com.example.inventarisapp.authentication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.inventarisapp.R
+import com.example.inventarisapp.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
